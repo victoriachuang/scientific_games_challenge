33d881e4cd52d6d887a46457cd8249a20ebd2bc8
victoriachuang <viiictoriachuang@gmail.com>
2016-04-11 18:08:15 -0500

    initial commit, pseudocode challenge requirements

---
+++ birth_year_population.py
@@ -0,0 +1,12 @@
+# write to file: name, birth year, death year
+
+
+
+# "tally" years that each person was alive
+
+# traverse through list of birth year and death year
+
+# traverse through hash
+# if key >= birth year and key <= death year, key[i]++
+
+# return key with max value
