436b051523c97f95a69f9f0d6e94e2573ae34971
victoriachuang <viiictoriachuang@gmail.com>
2016-04-11 19:16:18 -0500

    iterate through multiple jsons, return year with max value

--- birth_year_population.py
+++ birth_year_population.py
@@ -1,4 +1,5 @@
 import json
+import operator
 
 # write to file: name, birth year, death year
 
@@ -6,17 +7,17 @@ import json
 with open('data_set.json') as data_file:
 	data = json.load(data_file)
 
-# create dictionary => key is each year
+# create dictionary => key is each year, initialize each value to 0
 birth_year_tally = {}
 for i in range(1900, 2001):
 	birth_year_tally[i] = 0
 
 # "tally" years that each person was alive
-# traverse through list of birth year and death year
-# traverse through hash
-# if key >= birth year and key <= death year, key[i]++
-for key in birth_year_tally:
-	if key >= data["birth_year"] and key <= data["death_year"]:
-		birth_year_tally[key] += 1
+for person in data:
+	for key in birth_year_tally:
+		if key >= person["birth_year"] and key <= person["death_year"]:
+			birth_year_tally[key] += 1
 
 # return key with max value
+max_year = max(birth_year_tally.iteritems(), key=operator.itemgetter(1))[0]
+print max_year
