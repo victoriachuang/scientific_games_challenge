1d33be0301183b0700f080a0f31caabee8a8fc1c
victoriachuang <viiictoriachuang@gmail.com>
2016-04-11 20:13:36 -0500

    create data set with random birth and end years, returns year with max value

--- data_set_generation.py
+++ data_set_generation.py
@@ -1,12 +1,18 @@
 import json
+import random
 
 # write to file: name, birth year, death year
 # generate birth and death years between 1900 and 2000
 # death year must be equal to or after birth year
 with open("data.json", "w") as outfile:
+	outfile.write("[")
 	for i in range(0, 1000):	
 		start = int((random.random() * 100)) + 1900
 		end = int(((2000 - start) * random.random())) + start
 
 		# need to separate each object
 		json.dump({"name": "hello kale chips", "birth_year": start, "death_year": end}, outfile, indent=4)
+		if i < 999:
+			outfile.write(",\n")
+		else:
+			outfile.write("]")
