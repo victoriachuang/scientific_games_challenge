ca8a9449c4308fd55ae9027c68c8404f69a7de39
victoriachuang <viiictoriachuang@gmail.com>
2016-04-11 19:44:32 -0500

    clean up comments

--- birth_year_population.py
+++ birth_year_population.py
@@ -9,25 +9,21 @@ import operator
 start = int((random.random() * 100)) + 1900
 end = int(((2000 - start) * random.random())) + start
 
-print start
-print end
-# print end
-
 # open json file
-# with open('data_set.json') as data_file:
-# 	data = json.load(data_file)
+with open('data_set.json') as data_file:
+	data = json.load(data_file)
 
 # # create dictionary => key is each year, initialize each value to 0
-# birth_year_tally = {}
-# for i in range(1900, 2001):
-# 	birth_year_tally[i] = 0
+birth_year_tally = {}
+for i in range(1900, 2001):
+	birth_year_tally[i] = 0
 
 # # "tally" years that each person was alive
-# for person in data:
-# 	for key in birth_year_tally:
-# 		if key >= person["birth_year"] and key <= person["death_year"]:
-# 			birth_year_tally[key] += 1
+for person in data:
+	for key in birth_year_tally:
+		if key >= person["birth_year"] and key <= person["death_year"]:
+			birth_year_tally[key] += 1
 
 # # return key with max value
-# max_year = max(birth_year_tally.iteritems(), key=operator.itemgetter(1))[0]
-# print max_year
+max_year = max(birth_year_tally.iteritems(), key=operator.itemgetter(1))[0]
+print max_year
